1e09484128503a20d00045123ca7c3d1fbec558a
Jean-Hugues Roy <jh@jhroy.ca>
2020-01-28 18:37:57 -0500

    Create devoir1-JHR.py
    Bonjour Mayssa!
    Tu trouveras ma correction dans le fichier "devoir1-JHR.py"

---
+++ devoir1-JHR.py
@@ -0,0 +1,24 @@
+### BONJOUR MAYSSA, ICI JEAN-HUGUES ROY
+### TOUS MES COMMENTAIRES SERONT EN MAJUSCULES ET PRÉCÉDÉS DE 3 # (HASHTAGS)
+
+#2020 - Mayssa Ferah
+# coding: utf-8
+
+url="https://montrealcampus.ca?p="
+#print(url)
+l1=list(range(20000,30151))
+#print(l1)
+#20000 et 30150
+#pr chaque film au sing. ds les films au pluriel
+urlS= [] 
+for nb in l1:
+    #print(url+str(nb))
+    urlS.append(url+str(nb))
+#sortir boucle backspace
+print(urlS)
+print(len(urlS))
+
+### BRAVO D'ÊTRE VENUE ME POSER DES QUESTIONS. C'EST UNE DÉMARCHE TOUT À FAIT JOURNALISTIQUE QUI CONSISTE À CHERCHER À COMPRENDRE! :)
+### JE ME SOUVIENS QUE TU ÉTAIS VRAIMENT PRÈS DU BUT. SUPER!
+
+### PRENDS SIMPLEMENT L'HABITUDE D'ÉCRIRE PLUS DE COMMENTAIRES DANS TON CODE POUR AIDER LES GENS AVEC QUI TU TRAVAILLES DE COMPRENDRE CE QUE FAIT TON SCRIPT.
